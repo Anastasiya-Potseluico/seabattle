c10d8aaa5870fee83d85036915564afc6f7a62c9
Анастасия Поцелуйко < >
2016-05-13 23:37:45 +0300

    Добавлены методы для получения сообщений с координатами от врага, а также для получения результата выстрела.

--- src/seabattle/Player.java
+++ src/seabattle/Player.java
@@ -29,11 +29,11 @@ public class Player extends Agent{
 
     // 0 - неизвестная клетка
     // 1 - убитая клетка
-    // -1 - в такой клетке точно ничего не может быть
-    private int[][] _playField = new int[10][10];
+    // -1 - пустая клетка
     private int[][] _enemyField = new int[10][10];
-    private int _myKilledShipCount = 0;
-    private HashMap<Integer, Integer> _damagedShip = new HashMap<>();
+    // 0 - пустая клетка
+    // 1 - клетка с кораблем
+    private int[][] _playField = new int[10][10];
 
     @Override
     protected void setup() {
@@ -55,11 +55,12 @@ public class Player extends Agent{
                 _enemyField[i][j] = 0;
             }
         }
-        addBehaviour(new EnemyFire());
-        addBehaviour(new MyFire());
+        addBehaviour(new GetResponseFromEnemy());
+        addBehaviour(new MakeFire());
     }
 
-    class MyFire extends OneShotBehaviour {
+    // Класс атаки на вражескую клетку
+    class MakeFire extends OneShotBehaviour {
 
         private AID _enemyAgent = null;
         private int _hitXCoordinate = -1;
@@ -67,27 +68,106 @@ public class Player extends Agent{
 
         @Override
         public void action() {
-            // check coordinate x, y, check game over.
-        }
+            // Если нет убитых клеток на поле врага - отправить любую пустую клетку, иначе
+            // Отправить две координаты на основании правил
 
+        }
     }
 
-    class EnemyFire extends CyclicBehaviour {
+    // Класс для отправки результата атаки на свою клетку врагу
+    class SendResponseToEnemy extends OneShotBehaviour {
+
+        private AID _enemyAgent = null;
+        private MessageTemplate _mt;
+
+        public SendResponseToEnemy(AID enemyAgent) {
+            super();
+            _enemyAgent = enemyAgent;
+        }
+
+        // Метод для проверки попадания врага в свой корабль
+        private void AnalyzeEnemyAttack(int x, int y) {
+            if(_playField[x][y] == 1) {
+                // Отправить факт hit или ship
+            } else {
+                // Отправить сообщение о промахе
+                ACLMessage missMessage = new ACLMessage(ACLMessage.INFORM);
+                missMessage.addReceiver(_enemyAgent);
+                missMessage.setConversationId("getLatestMessages");
+                missMessage.setContent("miss");
+                _mt = MessageTemplate.and(
+                        MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+                        MessageTemplate.MatchConversationId("shotResult")
+                );
+                myAgent.send(missMessage);
+                System.out.println("Sent miss message");
+            }
+        }
+
+        @Override
+        public void action() {
+
+        }
+    }
+
+    // Класс для получения сообщения с результатом атаки на вражескую клетку
+    class GetResponseFromEnemy extends CyclicBehaviour {
         MessageTemplate _mt = MessageTemplate.and(
-                MessageTemplate.MatchPerformative(ACLMessage.REQUEST),
-                MessageTemplate.MatchConversationId("getLatestMessages")
+                MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+                MessageTemplate.MatchConversationId("shotResult")
         );
+
         @Override
         public void action() {
             ACLMessage msg = myAgent.receive(_mt);
             if (msg != null) {
-                Long timestamp = Long.parseLong(msg.getContent());
-                System.out.print(timestamp);
+                String fireState = msg.getContent();
+                System.out.print(fireState);
+                switch (fireState) {
+                    case "miss":
+
+                        break;
+                    case "hit":
+
+                        break;
+                    case "ship":
+                        // Пометить клетки вокруг корабля как пустые
+
+                        // Изменить базу фактов в jess о состоянии поля противника
+
+                        break;
+                }
             } else {
                 block();
             }
-            // check coordinate x, y, check game over.
         }
     }
 
+    // Класс для получения сообщения с координатами, выбранными врагом для атаки
+    class GetCoordinatesFromEnemy extends CyclicBehaviour {
+        private  int _x;
+        private  int _y;
+        // Передаем координаты в формате x,y
+        MessageTemplate _mt = MessageTemplate.and(
+                MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+                MessageTemplate.MatchConversationId("hitCoordinates")
+        );
+
+        @Override
+        public void action() {
+            ACLMessage msg = myAgent.receive(_mt);
+            if (msg != null) {
+               // parse coordinates
+                String[] coordinates = msg.getContent().split(",");
+                if(coordinates.length == 2) {
+                    _x = Integer.parseInt(coordinates[0]);
+                    _y = Integer.parseInt(coordinates[1]);
+                } else {
+                    System.out.println("Only two coordinates are available to be sent!");
+                }
+            } else {
+                block();
+            }
+        }
+    }
 }
